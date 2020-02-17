a86235da159326a0785bf43bae71cd9d5af352e5
Anjan kumar <anjan0103@gmail.com>
2020-02-16 20:56:44 -0500

    newly added checkout

--- shop/views.py
+++ shop/views.py
@@ -21,6 +21,10 @@ def index(request):
     return render(request, 'shop/index.html', {'product_objects': product_objects})
 
 
-def detail(request, id):
+def detail(request,id):
     product_object = Products.objects.get(id=id)
     return render(request, 'shop/detail.html', {'product_object': product_object})
+
+
+def checkout(request):
+    return render(request,'shop/checkout.html')
